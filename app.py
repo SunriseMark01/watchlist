9b4a4f46610ff54f88f20d20c9b47694739d1653
Xiaoteng Ma <mark_ma01@qq.com>
2024-02-03 11:06:51 +0800

    添加/index网页

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from markupsafe import escape
 
-from flask import Flask, url_for
+from flask import Flask, url_for,render_template
 
 app = Flask(__name__)
 
@@ -26,3 +26,23 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html', name=name, movies=movies)
